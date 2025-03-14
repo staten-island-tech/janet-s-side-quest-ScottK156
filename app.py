559aa3d76f649dc2a714dff87cb05a8b31482339
ScottK156 <scottk8@nycstudents.net>
2025-03-14 09:24:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,63 @@ file_path = "SalesData.csv"
 data = csv_to_list(file_path)
 """ print(data)   # Output the list """
 #task number 1
+""" def calcRow(data):
+    row_totals = {}
+    row_averages = {}
+    rounded_averages ={}
+    x = -1
+    for i in data[1]:
+        x += 1
+        print(x)
+
+    for row in data[1:]:  # Skipping the first row
+        store_name = row[0]  # First column is the store name
+        sales = map(int, row[1:])  # Convert sales to numbers
+        row_totals = sum(sales)  # Sum up sales for the store
+        row_averages = row_totals / x
+        rounded_averages[store_name] = round(row_averages,2)
+
+
+
+    return rounded_averages
+
+totals = calcRow(data)
+print(totals) """
+#task number 2
+def calcRow(data):
+    row_totals = {}
+    row_averages = {}
+    rounded_averages ={}
+    profit = []
+    x = -1
+    for i in data[1]:
+        x += 1
+
+    for row in data[1:]:  # Skipping the first row
+        store_name = row[0]  # First column is the store name
+        sales = map(int, row[1:])  # Convert sales to numbers
+        row_totals = sum(sales)  # Sum up sales for the store
+        row_averages = row_totals / x
+        rounded_averages[store_name] = round(row_averages,2)
+    for store in rounded_averages:
+        profit.append(store)
+    for store in profit:
+        g = max(profit)
+        print(g)
+        profit.remove(g)
+
+
+    
+        
+    
+       
+
+
+
+    
+
+totals = calcRow(data)
+print(totals)
 
 
 
