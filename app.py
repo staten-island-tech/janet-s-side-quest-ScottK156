3cfdc32959b7f1a693bd36bdf8d95494eec110c7
ScottK156 <scottk8@nycstudents.net>
2025-03-19 09:25:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ file_path = "SalesData.csv"
 data = csv_to_list(file_path)
 """ print(data)   # Output the list """
 #task number 1
-""" def calcRow(data):
+def calcRow(data):
     row_totals = {}
     row_averages = {}
     rounded_averages ={}
     x = -1
     for i in data[1]:
         x += 1
-        print(x)
+
 
     for row in data[1:]:  # Skipping the first row
         store_name = row[0]  # First column is the store name
@@ -36,9 +36,9 @@ data = csv_to_list(file_path)
     return rounded_averages
 
 totals = calcRow(data)
-print(totals) """
+print(totals)
 #task number 2
-def calcRow(data):
+""" def calcRow(data):
     row_totals = {}
     row_averages = {}
     rounded_averages ={}
@@ -55,24 +55,42 @@ def calcRow(data):
         rounded_averages[store_name] = round(row_averages,2)
     for store in rounded_averages:
         profit.append(store)
+        print(profit)
     for store in profit:
         g = max(profit)
         print(g)
-        profit.remove(g)
+        profit.remove(g) """
+
+""" totals = calcRow(data)
+print(totals) """
+#task number 3
+def calcRow(data):
+    row_totals = {}
+    row_averages = {}
+    rounded_averages ={}
+    x = -1
+    for i in data[1]:
+        x += 1
 
 
-    
+    for row in data[1:]:  # Skipping the first row
+        store_name = row[0]  # First column is the store name
+        sales = map(int, row[1:])  # Convert sales to numbers
+        row_totals = sum(sales)  # Sum up sales for the store
+        row_averages = row_totals / x
+        rounded_averages[store_name] = round(row_averages,2)
+    for i, value in rounded_averages.items():
         
-    
-       
+        print(all_totals)
+        
+   
 
 
 
-    
+    return rounded_averages
 
 totals = calcRow(data)
 print(totals)
-
 
 
 
